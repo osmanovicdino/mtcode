c1bed5c222b3cef1fb74c4cd9a336d1f93a51833
Dino Osmanovic <osmanovic.dino@gmail.com>
2021-02-03 14:32:11 -0500

    minor update

--- Code/Basic/Scripts/submit_job_single.sh
+++ Code/Basic/Scripts/submit_job_single.sh
@@ -5,9 +5,10 @@
 #$ -o joblog.$JOB_ID
 #$ -j y
 ## Edit the line below as needed:
-#$ -l h_rt=6:00:00,h_data=1G
+#$ -l h_rt=24:00:00,h_data=1G
 ## Modify the parallel environment
 ## and the number of cores as needed:
+#$ -pe shared 36
 # Email address to notify
 #$ -M $USER@mail
 # Notify when
@@ -27,11 +28,15 @@ module load gcc/4.9.3
 ## in the two lines below:
 ##echo '/usr/bin/time -v hostname'
 ##/usr/bin/time -v hostname
+den=0.01
+i1=15.
+i2=20.
+i3=18.
 dirwemake="den=${den}_i1=${i1}_i2=${i2}_i3=${i3}"
 mkdir /u/scratch/d/dinoo/PhaseDiagramBivalent/${dirwemake}
-cp ~/Chemistry/Code/main.cpp ~/Chemistry/Results1/
-g++ -std=c++11 -pg -no-pie ~/Chemistry/Code/main.cpp -o ~/Chemistry/Results1/angron
-cd ~/Chemistry/Results1/
+cp ~/Chemistry/Code/main.cpp /u/scratch/d/dinoo/PhaseDiagramBivalent/${dirwemake}
+g++ -std=c++11 ~/Chemistry/Code/main.cpp -o /u/scratch/d/dinoo/PhaseDiagramBivalent/${dirwemake}/angron
+cd /u/scratch/d/dinoo/PhaseDiagramBivalent/${dirwemake}
 ./angron $den $i1 $i2 $i3 >log
 # echo job info on joblog:
 echo "Job $JOB_ID ended on:   " `hostname -s`
