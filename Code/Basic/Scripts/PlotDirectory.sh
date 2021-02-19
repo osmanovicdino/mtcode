54074868d32756c3aa40f72903435754619481d6
Dino Osmanovic <osmanovic.dino@gmail.com>
2021-02-19 08:31:21 -0500

    minor update

--- Code/Basic/Scripts/PlotDirectory.sh
+++ Code/Basic/Scripts/PlotDirectory.sh
@@ -5,7 +5,7 @@
 #$ -o joblog.$JOB_ID
 #$ -j y
 ## Edit the line below as needed:
-#$ -l h_rt=12:00:00,h_data=8G
+#$ -l h_rt=12:00:00,h_data=16G
 ## Modify the parallel environment
 ## and the number of cores as needed:
 # Email address to notify
