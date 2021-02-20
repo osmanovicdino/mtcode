c3a5abb1709b7f89bc898be003be1b63aa0c5812
Dino Osmanovic <osmanovic.dino@gmail.com>
2021-02-20 09:14:51 -0500

    minor update

---
+++ Code/Basic/Scripts/MakeMovies.sh
@@ -0,0 +1,53 @@
+#### submit_job.sh START ####
+#!/bin/bash
+#$ -cwd
+# error = Merged with joblog
+#$ -o joblog.$JOB_ID
+#$ -j y
+## Edit the line below as needed:
+#$ -l h_rt=00:30:00,h_data=8G
+## Modify the parallel environment
+## and the number of cores as needed:
+# Email address to notify
+#$ -M $USER@mail
+# Notify when
+#$ -m bea
+#$ -t 1-50:1
+
+# echo job info on joblog:
+echo "Job $JOB_ID started on:   " `hostname -s`
+echo "Job $JOB_ID started on:   " `date `
+echo " "
+
+# load the job environment:
+. /u/local/Modules/default/init/modules.sh
+## Edit the line below as needed:
+module load gcc/7.5.0
+module load mathematica/12.1
+module load ffmpeg
+
+if [ -e  ~/Chemistry/Code/Basic/Scripts/directories.txt ]; then
+   # use the unix command sed -n ${line_number}p to read by line
+   dir=`sed -n ${SGE_TASK_ID}p ~/Chemistry/Code/Basic/Scripts/directories.txt`
+   echo "read file correctly" 
+else
+   dir="/u/scratch/d/dino/"
+   echo "did not read file correctly"
+fi
+## substitute the command to run your code
+## in the two lines below:
+##echo '/usr/bin/time -v hostname'
+##/usr/bin/time -v hostname
+##mkdir ~/Chemistry/Results1
+##cp ~/Chemistry/Code/main.cpp ~/Chemistry/Results1/
+##g++ -fopenmp -std=c++11 ~/Chemistry/Code/main.cpp -o ~/Chemistry/Results1/angron
+##cd ~/Chemistry/Results1/
+##./angron
+cd $dir
+ffmpeg -i pos_beta\=1_i\=%05d.jpg -vcodec libx264 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -pix_fmt yuv420p test.mp4
+
+# echo job info on joblog:
+echo "Job $JOB_ID ended on:   " `hostname -s`
+echo "Job $JOB_ID ended on:   " `date `
+echo " "
+#### submit_job.sh STOP ####
