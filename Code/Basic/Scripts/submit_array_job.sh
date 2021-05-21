55fac4ed461ea96e9b85e504802865330c9a952c
Dino Osmanovic <osmanovic.dino@gmail.com>
2021-05-21 11:52:03 -0700

    minor update

--- Code/Basic/Scripts/submit_array_job.sh
+++ Code/Basic/Scripts/submit_array_job.sh
@@ -13,7 +13,7 @@
 #$ -M $USER@mail
 # Notify when
 #$ -m bea
-#$ -t 1-16:1
+#$ -t 1-36:1
 
 # echo job info on joblog:
 echo "Job $JOB_ID started on:   " `hostname -s`
@@ -29,7 +29,7 @@ module load gcc/7.5.0
 ## in the two lines below:
 ##echo '/usr/bin/time -v hostname'
 ##/usr/bin/time -v hostname
-filename=~/Chemistry/Code/Basic/Scripts/params15.dat
+filename=~/Chemistry/Code/Basic/Scripts/params16.dat
 if [ -e ${filename}   ]; then
    # use the unix command sed -n ${line_number}p to read by line
    den=`sed -n ${SGE_TASK_ID}p ${filename} | awk '{print $1}'`
