c47080715d9655b9cbc8ff60db1f20e320d0a328
osmanovicdino <osmanovic.dino@gmail.com>
2021-06-11 14:00:16 -0700

    minor update

--- Code/Basic/Scripts/submit_array_job.sh
+++ Code/Basic/Scripts/submit_array_job.sh
@@ -29,7 +29,7 @@ module load gcc/7.5.0
 ## in the two lines below:
 ##echo '/usr/bin/time -v hostname'
 ##/usr/bin/time -v hostname
-filename=~/Chemistry/Code/Basic/Scripts/params21.dat
+filename=~/Chemistry/Code/Basic/Scripts/params22.dat
 basedir="PhaseDiagramBivalent"
 if [ -e ${filename}   ]; then
    # use the unix command sed -n ${line_number}p to read by line
