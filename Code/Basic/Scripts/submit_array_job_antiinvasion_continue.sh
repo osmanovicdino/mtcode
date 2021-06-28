92e3a588a04c108f33cb6b314969de2098c4fb5b
osmanovicdino <osmanovic.dino@gmail.com>
2021-06-28 12:30:58 -0700

    minor update

--- Code/Basic/Scripts/submit_array_job_antiinvasion_continue.sh
+++ Code/Basic/Scripts/submit_array_job_antiinvasion_continue.sh
@@ -29,7 +29,7 @@ module load gcc/9.3.0
 ## in the two lines below:
 ##echo '/usr/bin/time -v hostname'
 ##/usr/bin/time -v hostname
-filename=~/Chemistry/Code/Basic/Scripts/params2.dat
+filename=~/Chemistry/Code/Basic/Scripts/params3.dat
 basedir="PhaseDiagramBivalent"
 if [ -e ${filename}   ]; then
    # use the unix command sed -n ${line_number}p to read by line
