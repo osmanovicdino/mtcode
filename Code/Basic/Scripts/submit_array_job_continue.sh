92684e7bdaf83197956ee835fd9754942871632e
osmanovicdino <osmanovic.dino@gmail.com>
2021-08-07 13:04:01 -0700

    minor update

--- Code/Basic/Scripts/submit_array_job_continue.sh
+++ Code/Basic/Scripts/submit_array_job_continue.sh
@@ -13,7 +13,7 @@
 #$ -M $USER@mail
 # Notify when
 #$ -m bea
-#$ -t 1-24:1
+#$ -t 1-12:1
 
 # echo job info on joblog:
 echo "Job $JOB_ID started on:   " `hostname -s`
@@ -29,8 +29,9 @@ module load gcc/9.3.0
 ## in the two lines below:
 ##echo '/usr/bin/time -v hostname'
 ##/usr/bin/time -v hostname
-direcres="PhaseDiagramDesign9"
-filename=~/Chemistry/Code/Basic/Scripts/params13.dat
+filename=~/Chemistry/Code/Basic/Scripts/params14.dat
+direcres="PhaseDiagramDesign10"
+
 if [ -e ${filename}   ]; then
    # use the unix command sed -n ${line_number}p to read by line
    den=`sed -n ${SGE_TASK_ID}p ${filename} | awk '{print $1}'`
