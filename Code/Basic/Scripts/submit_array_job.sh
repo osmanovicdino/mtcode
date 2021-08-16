3f616c3021b7f967169065cf8c5c37cb5c8e7c2e
osmanovicdino <osmanovic.dino@gmail.com>
2021-08-16 13:36:34 -0700

    minor update

--- Code/Basic/Scripts/submit_array_job.sh
+++ Code/Basic/Scripts/submit_array_job.sh
@@ -29,8 +29,8 @@ module load gcc/9.3.0
 ## in the two lines below:
 ##echo '/usr/bin/time -v hostname'
 ##/usr/bin/time -v hostname
-filename=~/Chemistry/Code/Basic/Scripts/params18.dat
-basedir="PhaseDiagramDesign13"
+filename=~/Chemistry/Code/Basic/Scripts/params19.dat
+basedir="PhaseDiagramDesign14"
 if [ -e ${filename}   ]; then
    # use the unix command sed -n ${line_number}p to read by line
    den=`sed -n ${SGE_TASK_ID}p ${filename} | awk '{print $1}'`
