ecf9aa997adedcc98aa8a1a31bb6914c6788bcb8
osmanovicdino <osmanovic.dino@gmail.com>
2021-09-06 14:13:29 -0700

    minor update

--- Code/Basic/Scripts/submit_array_job_random.sh
+++ Code/Basic/Scripts/submit_array_job_random.sh
@@ -23,7 +23,7 @@ module load gcc/9.3.0
 ##echo '/usr/bin/time -v hostname'
 ##/usr/bin/time -v hostname
 filename=~/Chemistry/Code/Basic/Scripts/paramsrandoms.dat
-basedir="PhaseDiagramDesignAll2"
+basedir="PhaseDiagramDesignAll"
 if [ -e ${filename}   ]; then
    # use the unix command sed -n ${line_number}p to read by line
    den=`sed -n ${SGE_TASK_ID}p ${filename} | awk '{print $1}'`
