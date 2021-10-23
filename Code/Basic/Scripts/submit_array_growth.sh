5190ab8b8cd52b08822366ff7910b22500167eb8
osmanovicdino <osmanovic.dino@gmail.com>
2021-10-23 12:42:08 -0700

    minor update

--- Code/Basic/Scripts/submit_array_growth.sh
+++ Code/Basic/Scripts/submit_array_growth.sh
@@ -29,7 +29,7 @@ module load gcc/9.3.0
 ## in the two lines below:
 ##echo '/usr/bin/time -v hostname'
 ##/usr/bin/time -v hostname
-filename=~/Chemistry/Code/Basic/Scripts/paramsgrowth2.dat
+filename=~/Chemistry/Code/Basic/Scripts/paramsgrowth3.dat
 basedir="GrowthRun3"
 if [ -e ${filename}   ]; then
    # use the unix command sed -n ${line_number}p to read by line
