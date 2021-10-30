39a53dcd10f20bb4a29ff714144e104c82ba2167
osmanovicdino <osmanovic.dino@gmail.com>
2021-10-30 14:47:57 -0700

    minor update

--- Code/Basic/Scripts/submit_array_growth2.sh
+++ Code/Basic/Scripts/submit_array_growth2.sh
@@ -8,7 +8,7 @@
 #$ -l h_rt=12:00:00,h_data=256M
 ## Modify the parallel environment
 ## and the number of cores as needed:
-#$ -pe shared 8
+#$ -pe shared 4
 # Email address to notify
 #$ -M $USER@mail
 # Notify when
@@ -48,7 +48,7 @@ mkdir /u/scratch/d/dinoo/${basedir}/${dirwemake}
 cp ~/Chemistry/Code/mainGas.cpp /u/scratch/d/dinoo/${basedir}/${dirwemake}
 g++ -fopenmp ~/Chemistry/Code/mainGas.cpp -o /u/scratch/d/dinoo/${basedir}/${dirwemake}/angron
 cd /u/scratch/d/dinoo/${basedir}/${dirwemake}
-export OMP_NUM_THREADS=8
+export OMP_NUM_THREADS=4
 ./angron $den >log
 # echo job info on joblog:
 echo "Job $JOB_ID ended on:   " `hostname -s`
