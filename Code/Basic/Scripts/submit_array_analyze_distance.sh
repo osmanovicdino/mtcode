d8b42334c6387b5ba1e7ed9e065b2e7cbf434075
osmanovicdino <osmanovic.dino@gmail.com>
2021-11-09 18:40:56 -0800

    minor update

--- Code/Basic/Scripts/submit_array_analyze_distance.sh
+++ Code/Basic/Scripts/submit_array_analyze_distance.sh
@@ -9,7 +9,7 @@
 ## Modify the parallel environment
 ## and the number of cores as needed:
 #$ -pe shared 8
-#$ -t 1-11:1
+#$ -t 1-5:1
 
 # echo job info on joblog:
 echo "Job $JOB_ID started on:   " `hostname -s`
@@ -25,8 +25,8 @@ module load gcc/10.2.0
 ## in the two lines below:
 ##echo '/usr/bin/time -v hostname'
 ##/usr/bin/time -v hostname
-filename=~/Chemistry/Code/Basic/Scripts/paramsgrowth10analyze.dat
-basedir="GrowthRun7"
+filename=~/Chemistry/Code/Basic/Scripts/paramsgrowth11analyze.dat
+basedir="GrowthRun6"
 if [ -e ${filename}   ]; then
    # use the unix command sed -n ${line_number}p to read by line
    strdir=`sed -n ${SGE_TASK_ID}p ${filename} | awk '{print $1}'`
