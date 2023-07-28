4a24fa2cd96d9bf69b7ce8350f16290776bad52b
osmanovicdino <osmanovic.dino@gmail.com>
2023-07-28 15:42:46 -0700

    minor update

--- Code/Basic/Scripts/continue_run.sh
+++ Code/Basic/Scripts/continue_run.sh
@@ -7,7 +7,7 @@ directory_path=$1
 g++ -fopenmp -std=c++17  ~/Chemistry/Code/mainNanotubeElasticShell_import.cpp -o ${directory_path}/a #copy to the directory we want 
 fileparam=./params.csv
 posfile=$(ls -t ${directory_path}/pos* | head -n 1)
-orifile=$(ls -t ${directory_path}/ori* | head -n 1)
+orifile=./orient.csv
 indfile=$(ls -t ${directory_path}/div* | head -n 1)
 pfile=./IsocohedronI.csv
 #cp $pfile ${directory_path}
