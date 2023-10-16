1e602cf38e2bd178b3aabb9c306290ba16a5c8c6
osmanovicdino <osmanovic.dino@gmail.com>
2023-10-16 10:39:53 -0700

    minor update

---
+++ Code/Basic/Scripts/continue_run_pulsed.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# this code should be run from the base directory of the git
+
+directory_path=$1
+# param_file=$2 this file should be in the directory
+g++ -fopenmp -std=c++17  ~/Chemistry/Code/mainNanotubeElasticPulsedImport.cpp -o ${directory_path}/a #copy to the directory we want 
+fileparam=./param.csv
+#cp $pfile ${directory_path}
+echo $fileparam
+# echo $posfile
+# echo $orifile
+# echo $indfile
+# echo $pfile
+cd $directory_path
+export OMP_NUM_THREADS=8
+par=`ls -l pos* | wc -l`
+./a $fileparam $par >log
