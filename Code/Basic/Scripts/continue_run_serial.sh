5540bb32484aca96fc21bd90731b42aa85672d7f
osmanovicdino <osmanovic.dino@gmail.com>
2023-11-10 18:46:26 -0800

    minor update

---
+++ Code/Basic/Scripts/continue_run_serial.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# this code should be run from the base directory of the git
+
+directory_path=$1
+# param_file=$2 this file should be in the directory
+g++ -std=c++17  ~/Chemistry/Code/mainNanotubeElasticShell_import.cpp -o ${directory_path}/a #copy to the directory we want 
+fileparam=./param.csv
+posfile=$(ls -t ${directory_path}/pos* | head -n 1)
+orifile=./orient.csv
+indfile=$(ls -t ${directory_path}/div* | head -n 1)
+pfile=./IsocohedronI.csv
+#cp $pfile ${directory_path}
+echo $fileparam
+echo $posfile
+echo $orifile
+echo $indfile
+echo $pfile
+cd $directory_path
+./a $fileparam $posfile $orifile $indfile $pfile >log
