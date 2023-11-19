267528857382504d69fa2ab291b2f11d1abfa7ba
osmanovicdino <osmanovic.dino@gmail.com>
2023-11-18 19:30:02 -0800

    minor update

--- Code/Basic/Scripts/submit_array_job_start_pulsed.sh
+++ Code/Basic/Scripts/submit_array_job_start_pulsed.sh
@@ -5,11 +5,11 @@
 #$ -o joblog.$JOB_ID
 #$ -j y
 ## Edit the line below as needed:
-#$ -l h_rt=00:59:59,h_data=128M
+#$ -l h_rt=11:59:59,h_data=128M
 ## Modify the parallel environment
 ## and the number of cores as needed:
 #$ -pe shared 6
-#$ -t 1-100:1
+#$ -t 1-20:1
 
 # echo job info on joblog:
 echo "Job $JOB_ID started on:   " `hostname -s`
@@ -28,7 +28,7 @@ module load gcc/10.2.0
 #filename=~/Chemistry/Code/Basic/Scripts/paramsSA6.dat
 #basedir="SelfAssembly6"
 
-filename=~/Chemistry/Code/Basic/Scripts/parameter_settings11.dat
+filename=~/Chemistry/Code/Basic/Scripts/parameter_settings9.dat
 if [ -e ${filename}   ]; then
    # use the unix command sed -n ${line_number}p to read by line
    wt=`sed -n ${SGE_TASK_ID}p ${filename} | awk '{print $1}'`
@@ -37,16 +37,16 @@ else
    wt=~
    echo "did not read file correctly"
 fi
-basedir="SelfAssembly19"
+basedir="SelfAssembly17"
 dirwemake="try=${SGE_TASK_ID}"
 mkdir /u/scratch/d/dinoo/${basedir}/${dirwemake}
 cp ~/Chemistry/Code/mainNanotubeElasticShellPulsed.cpp /u/scratch/d/dinoo/${basedir}/${dirwemake}
 cp ~/Chemistry/Code/IsocohedronI.csv /u/scratch/d/dinoo/${basedir}/${dirwemake}
 cp ~/Chemistry/Code/IsocohedronP.csv /u/scratch/d/dinoo/${basedir}/${dirwemake}
 cp ~/Chemistry/Code/Basic/InitialConditions/${wt} /u/scratch/d/dinoo/${basedir}/${dirwemake}/param.csv
-#g++ -fopenmp -std=c++17 ~/Chemistry/Code/mainNanotubeElasticShellPulsed.cpp -o /u/scratch/d/dinoo/${basedir}/${dirwemake}/angron
-cp ~/Chemistry/Code/single_core_pulsed /u/scratch/d/dinoo/${basedir}/${dirwemake}/angron
+g++ -fopenmp -std=c++17 ~/Chemistry/Code/mainNanotubeElasticShellPulsed.cpp -o /u/scratch/d/dinoo/${basedir}/${dirwemake}/angron
 cd /u/scratch/d/dinoo/${basedir}/${dirwemake}
+export OMP_NUM_THREADS=6
 ./angron 'param.csv' >log
 # cp ~/Chemistry/Code/mainNanotubeElasticShell.cpp /u/scratch/d/dinoo/${basedir}/${dirwemake}
 # cp ~/Chemistry/Code/params.csv /u/scratch/d/dinoo/${basedir}/${dirwemake}
