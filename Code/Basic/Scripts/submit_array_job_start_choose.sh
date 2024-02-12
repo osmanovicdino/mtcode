cfc6e76c4a6e006dd9e498c6c970b95694c895c2
osmanovicdino <osmanovic.dino@gmail.com>
2024-02-12 11:39:28 -0800

    minor update

--- Code/Basic/Scripts/submit_array_job_start_choose.sh
+++ Code/Basic/Scripts/submit_array_job_start_choose.sh
@@ -28,24 +28,24 @@ module load gcc/10.2.0
 #filename=~/Chemistry/Code/Basic/Scripts/paramsSA6.dat
 #basedir="SelfAssembly6"
 
-filename=~/Chemistry/Code/Basic/Scripts/parameter_settings19.dat
+filename=~/Chemistry/Code/Basic/Scripts/parameter_settings20.dat
 if [ -e ${filename}   ]; then
    # use the unix command sed -n ${line_number}p to read by line
    wt=`sed -n ${SGE_TASK_ID}p ${filename} | awk '{print $1}'`
    wt2=`sed -n ${SGE_TASK_ID}p ${filename} | awk '{print $2}'`
-   echo "read file correctl y" 
+   echo "read file correctly" 
 else
    wt=~
    echo "did not read file correctly"
 fi
-basedir="SelfAssembly31"
+basedir="SelfAssembly32"
 dirwemake="try=${SGE_TASK_ID}"
 mkdir /u/scratch/d/dinoo/${basedir}/${dirwemake}
-cp ~/Chemistry/Code/mainNanotubeElasticShell.cpp /u/scratch/d/dinoo/${basedir}/${dirwemake}
+cp ~/Chemistry/Code/mainNanotubeElasticShell4.cpp /u/scratch/d/dinoo/${basedir}/${dirwemake}
 cp ~/Chemistry/Code/IsocohedronI.csv /u/scratch/d/dinoo/${basedir}/${dirwemake}
 cp ~/Chemistry/Code/${wt2} /u/scratch/d/dinoo/${basedir}/${dirwemake}/IsocohedronP.csv
 cp ~/Chemistry/Code/Basic/InitialConditions/${wt} /u/scratch/d/dinoo/${basedir}/${dirwemake}/param.csv
-g++ -fopenmp -std=c++17 ~/Chemistry/Code/mainNanotubeElasticShell.cpp -o /u/scratch/d/dinoo/${basedir}/${dirwemake}/angron
+g++ -fopenmp -std=c++17 ~/Chemistry/Code/mainNanotubeElasticShell4.cpp -o /u/scratch/d/dinoo/${basedir}/${dirwemake}/angron
 cd /u/scratch/d/dinoo/${basedir}/${dirwemake}
 export OMP_NUM_THREADS=6
 ./angron 'param.csv' >log
