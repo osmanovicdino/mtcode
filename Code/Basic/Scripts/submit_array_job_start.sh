d78646c5ead1860485370a66573356c2af00e616
osmanovicdino <osmanovic.dino@gmail.com>
2024-07-01 13:43:41 -0700

    minor update

--- Code/Basic/Scripts/submit_array_job_start.sh
+++ Code/Basic/Scripts/submit_array_job_start.sh
@@ -28,7 +28,7 @@ module load gcc/10.2.0
 #filename=~/Chemistry/Code/Basic/Scripts/paramsSA6.dat
 #basedir="SelfAssembly6"
 
-filename=~/Chemistry/Code/Basic/Scripts/parameter_settings1.dat
+filename=~/Chemistry/Code/Basic/Scripts/parameter_settings2.dat
 if [ -e ${filename}   ]; then
    # use the unix command sed -n ${line_number}p to read by line
    wt=`sed -n ${SGE_TASK_ID}p ${filename} | awk '{print $1}'`
@@ -37,7 +37,7 @@ else
    wt=~
    echo "did not read file correctly"
 fi
-basedir="SelfAssembly1"
+basedir="SelfAssembly2"
 dirwemake="try=${SGE_TASK_ID}"
 mkdir /u/scratch/d/dinoo/${basedir}/${dirwemake}
 cp ~/Chemistry/Code/mainNanotubeElasticShell.cpp /u/scratch/d/dinoo/${basedir}/${dirwemake}
