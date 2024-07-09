0a6ba81a7e715149a5f3b1b1ea7eee333222c817
osmanovicdino <osmanovic.dino@gmail.com>
2024-07-09 14:52:58 -0700

    minor update

--- Code/Basic/Scripts/submit_array_job_start.sh
+++ Code/Basic/Scripts/submit_array_job_start.sh
@@ -37,7 +37,7 @@ else
    wt=~
    echo "did not read file correctly"
 fi
-basedir="SelfAssembly4"
+basedir="SelfAssembly5"
 dirwemake="try=${SGE_TASK_ID}"
 mkdir /u/scratch/d/dinoo/${basedir}/${dirwemake}
 cp ~/Chemistry/Code/mainNanotubeElasticShell2.cpp /u/scratch/d/dinoo/${basedir}/${dirwemake}
