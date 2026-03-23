8ff877955505aae8a6a3fb6fddf344adacd0df5d
osmanovicdino <osmanovic.dino@gmail.com>
2026-03-23 14:30:51 -0700

    minor update

--- Code/Basic/Scripts/submit_array_job_genetic_next.sh
+++ Code/Basic/Scripts/submit_array_job_genetic_next.sh
@@ -27,7 +27,7 @@ module load gcc/11.3.0
 ##/usr/bin/time -v hostname
 
 dirwemake="den${SGE_TASK_ID}"
-subdir="GeneticTry12_3"
+subdir="GeneticTry12_4"
 cp ~/Chemistry/Code/mainNanotubeBoxImportEquil.cpp /u/scratch/d/dinoo/${subdir}/${dirwemake}
 g++ ~/Chemistry/Code/mainNanotubeBoxImportEquil.cpp -fopenmp -std=c++17 -o /u/scratch/d/dinoo/${subdir}/${dirwemake}/angron
 cd /u/scratch/d/dinoo/${subdir}/${dirwemake}
